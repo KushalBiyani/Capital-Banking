6fbdf541130972bf3b89962595aabc6be2594998
KushalBiyani <KushalBiyani@gmail.com>
2021-01-08 23:43:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Customer = require('./src/models/customer');
 const Transaction = require('./src/models/transaction');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'bank-client/build')));
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'bank-client/build', 'index.html'));
 });
 var jsonParser = bodyParser.json()
