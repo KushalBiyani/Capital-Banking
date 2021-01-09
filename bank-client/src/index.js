b29157d8810082e76b706bb9bbe13f4229a6e0f7
KushalBiyani <KushalBiyani@gmail.com>
2021-01-09 12:22:03 +0530

    error fixing part -2

--- bank-client/src/index.js
+++ bank-client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
